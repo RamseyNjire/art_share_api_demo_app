de886de56ec5eafc5647833b7dfd2709a4f9f7cf
Ramsey Njire <ramseynjire@gmail.com>
2021-05-28 02:04:13 +0300

    seed artworks

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,10 @@ reson = User.create(username: "Reson")
 gideon = User.create(username: "Gideon")
 cecilia = User.create(username: "Cecilia")
 brian = User.create(username: "Brian")
+wamucii = User.create(username: "Wamucii")
+
+mushroom = Artwork.create(title: "Mushroom", image_url: "first_url", artist_id: ramsey.id)
+cactus = Artwork.create(title: "Cactus", image_url: "second_url", artist_id: reson.id)
+hotdog = Artwork.create(title: "Hotdog", image_url: "third_url", artist_id: wamucii.id)
+bee = Artwork.create(title: "Bee", image_url: "third_url", artist_id: reson.id)
+
