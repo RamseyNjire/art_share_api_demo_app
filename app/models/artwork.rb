c2a0f676925a8e1512b81a735299152c3229490e
Ramsey Njire <ramseynjire@gmail.com>
2021-05-28 01:49:16 +0300

    add associations between artworks and users

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,2 +1,15 @@
 class Artwork < ApplicationRecord
+    validates :title, :image_url, presence: true
+    validates :image_url, uniqueness: true
+    validates :title, uniqueness: {
+        scope: :artist_id,
+        message: "cannot be same for two artworks by the same artist"
+    }
+
+    belongs_to(
+        :artist,
+        class_name: 'User',
+        foreign_key: :artist_id,
+        primary_key: :id
+    )
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
+
+    has_many(
+        :artworks,
+        class_name: 'Artwork',
+        foreign_key: :artist_id,
+        primary_key: :id,
+        dependent: :destroy
+    )
 end
