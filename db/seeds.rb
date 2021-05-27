edfc249c0ed8ee7a857f2e3aaeeb94f9988063a6
Ramsey Njire <ramseynjire@gmail.com>
2021-05-27 23:58:20 +0300

    seed database with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ramsey = User.create()
+ramsey = User.create(username: "Ramsey")
+reson = User.create(username: "Reson")
+gideon = User.create(username: "Gideon")
+cecilia = User.create(username: "Cecilia")
+brian = User.create(username: "Brian")
